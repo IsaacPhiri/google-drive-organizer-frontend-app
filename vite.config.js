a44360791bc10fb134472bdd491a37a6440b2537
Isaac Phiri <isaacphiri315@gmail.com>
2025-07-25 07:48:46 +0200

    feat: connect frontend to backend upload endpoint using await fetch

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,15 @@ export default defineConfig({
 
   server: {
     // frontend
-    port: 3000,
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000,
 
     // backend server
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'https://google-drive-organizer.onrender.com',
       }
     },
   },
 })
+
